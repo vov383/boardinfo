cca2058258fa8b527002b5dec10ed754b0df9730
tlsalswo1004 <tlsalswo1004@naver.com>
2023-06-07 21:36:32 +0900

    230607 리뷰 입력시 로그인 아이디 적용 완료, 페이징 작업 중

---
+++ src/main/java/com/example/boardinfo/service/review/paging.java
@@ -0,0 +1,18 @@
+package com.example.boardinfo.service.review;
+
+public class paging {
+
+/*
+    public PagingVO(int total, int nowPage, int cntPerPage) {
+        setNowPage(nowPage);
+        setCntPerPage(cntPerPage);
+        setTotal(total);
+        calcLastPage(getTotal(), getCntPerPage());
+        calcStartEndPage(getNowPage(), cntPage);
+        calcStartEnd(getNowPage(), getCntPerPage());
+    }
+*/
+
+
+
+}
